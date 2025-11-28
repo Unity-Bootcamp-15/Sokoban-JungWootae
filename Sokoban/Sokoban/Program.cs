751969a20e1ef9d7c615d60661d609f890c45399
NullMa <jungwt524@gmail.com>
2025-11-28 16:01:20 +0900

    feat : add_Map_System

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -1,23 +1,49 @@
 ﻿namespace Sokoban {
     internal class Program {
         static void Main(string[] args) {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.CursorVisible = false;
+            int playerX = 3;
+            int playerY = 3;
 
-            int playerX = 0;
-            int playerY = 0;
+            ConsoleKeyInfo keyInfo;
+            int[,] mapData = new int[,]{       
+                // 0 : blank, 1 : wall, 2 : player, 3 : box, 4 : goal
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                { 1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1},
+                { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+            };
 
-            Console.SetCursorPosition(0, 0);
-            Console.Write("◈");
-
+            GameStart();
             while (true) {
-                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                keyInfo = Console.ReadKey(true);
+                PlayerReset();
+                PlayerMove();
+            }
+
+            void GameStart() {
+                Console.Clear();
+                CreateMap();
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.CursorVisible = false;
+                Console.Title = "(   {=+=[=Sokoban=]=+=}   )";
+                Console.SetCursorPosition(3, 3);
+                Console.Write("◈");
+            }
+
+            void PlayerMove() {
                 int moveX = playerX;
                 int moveY = playerY;
 
-                switch (keyInfo.Key)
-                {
+                switch (keyInfo.Key) {
                     case ConsoleKey.UpArrow:
+                        Console.SetCursorPosition(playerX, playerY);
+
+
                         if (playerY > 0)
                             moveY = playerY - 1;
                         break;
@@ -41,12 +67,38 @@
                 if (moveX != playerX || moveY != playerY) {
                     Console.SetCursorPosition(playerX, playerY);
                     Console.Write(" ");
-
                     playerX = moveX;
                     playerY = moveY;
-
                     Console.SetCursorPosition(playerX, playerY);
                     Console.Write("◈");
+                }
+            }
+
+            void PlayerReset() {
+                if (keyInfo.Key != ConsoleKey.R)
+                    return;
+                GameStart();
+            }
+            
+            void CreateMap() {
+                // 0 : blank, 1 : wall, 2 : player, 3 : box, 4 : goal
+                for (int i = 0; i < mapData.GetLength(0); i++) {
+                    for (int j = 0; j < mapData.GetLength(1); j++) {
+                        switch (mapData[i, j]) {
+                            case 0:
+                                Console.Write(" ");
+                                break;
+
+                            case 1:
+                                Console.Write("■");
+                                break;
+
+                            case 2:
+                                Console.Write("◈");
+                                break;
+                        }
+                    }
+                    Console.WriteLine();
                 }
             }
         }
