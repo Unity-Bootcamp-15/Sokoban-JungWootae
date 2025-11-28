c35ba27ec50350ae377c8dcfc2b0059d48ae4a9d
NullMa <jungwt524@gmail.com>
2025-11-28 10:47:28 +0900

    feat : add_Func_ResetGame

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -1,10 +1,18 @@
 ﻿namespace Sokoban
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("testing Commit");
+    internal class Program{
+        static void Main(string[] args){
+            
+            Console.ReadKey();
+        }
+
+        public void ResetGame() {
+            Console.ResetColor();
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Title = "Sokoban Game";
+            Console.CursorVisible = false;
+            Console.Clear();
         }
     }
 }
