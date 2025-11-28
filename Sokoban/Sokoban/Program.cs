2cbf08635b7d7a7d2cccce89cf594f9bca04419e
NullMa <jungwt524@gmail.com>
2025-11-28 12:32:46 +0900

    feat : add_player

--- Sokoban/Sokoban/Program.cs
+++ Sokoban/Sokoban/Program.cs
@@ -1,18 +1,57 @@
 ﻿namespace Sokoban
 {
     internal class Program{
-        static void Main(string[] args){
-            
-            Console.ReadKey();
-        }
 
-        public void ResetGame() {
-            Console.ResetColor();
-            Console.BackgroundColor = ConsoleColor.White;
-            Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.Title = "Sokoban Game";
+        static void Main(string[] args){
+            Console.ForegroundColor = ConsoleColor.Magenta;
             Console.CursorVisible = false;
-            Console.Clear();
+
+            int playerX = 0;
+            int playerY = 0;
+
+            Console.SetCursorPosition(0, 0);
+            Console.Write("◈");
+
+            while (true) {
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                int curPosX = Console.GetCursorPosition().Left;
+                int curPosY = Console.GetCursorPosition().Top;
+
+                int moveX = playerX;
+                int moveY = playerY;
+
+                switch (keyInfo.Key) {
+                    case ConsoleKey.UpArrow:
+                        if (playerY > 0)
+                            moveY = playerY - 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (playerY < Console.BufferHeight - 1)
+                            moveY = playerY + 1;
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        if (playerX > 0)
+                            moveX = playerX - 1;
+                        break;
+                    case ConsoleKey.RightArrow:
+                        if (playerX < Console.BufferWidth - 1)
+                            moveX = playerX + 1;
+                        break;
+
+                    case ConsoleKey.Escape:
+                        return;
+                }
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write(" ");
+
+                playerX = moveX;
+                playerY = moveY;
+
+                Console.SetCursorPosition(playerX, playerY);
+                Console.Write("◈");
+            }
         }
     }
 }
